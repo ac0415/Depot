ce0965a1264373ac9287d525873db2c8403c63f7
April Canlas <seemeseeyou0415@gmail.com>
2012-04-14 00:11:04 +0800

    Must edit stuff

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
+
+#The field’s title, description, and image URL are not empty.
+#The price is a valid number not less than $0.01.
+#The title is unique among all products.
+#The image URL looks reasonable.
+
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format:{
+    with:	%r{\.(gif|jpg|png)$}i,
+    message:	'must be a URL for GIF, JPG, or PNG image.'
+}
 end
